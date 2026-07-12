645356b422b8b2c16f5974369ab70661dfb01e2d
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-12 17:01:05 -0500

    Creación de proyecto Ingreso por Idioma Creación de carpetas Enlace de Git

---
+++ scripts/Enlace_Carpetas.R
@@ -0,0 +1,25 @@
+#=====================================================
+#Proyecto: INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 12-07-2026
+#====================================================
+
+#Creación de Carpetas------------------
+
+dir.create("Datos")
+dir.create("Datos/Crudos")
+dir.create("Datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+dir.create("scripts")
+
+
+#Enlace con Git y Github
+usethis::use_git_config(
+  user.name = "Hasbrali",
+  user.email = "esteba.raphael@pucp.edu.pe"
+)
+
+usethis::use_git()
+gitcreds::gitcreds_set()
+usethis::use_github()
