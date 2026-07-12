1fb8b13c398ab9c41cc79df56d785c70621b5cce
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-12 17:14:43 -0500

    Creación Script de Acondicionamiento de Datos Carga de librerias a usar en el script

---
+++ scripts/Acondicionamiento_Datos.R
@@ -0,0 +1,13 @@
+#=====================================================================================
+#Proyecto: INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 12-07-2026
+#Script: ACONDICIONAMIENTO DE DATOS (ENFOQUE: JEFES DE FAMILIA)
+#=====================
+
+#1.Carga de Librerias-----------------------------------------------------------
+library(tidyverse)
+library(arrow)
+library(janitor)
+library(naniar)
+renv::snapshot()
