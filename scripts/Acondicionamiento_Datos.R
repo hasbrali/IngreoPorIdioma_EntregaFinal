19ee7dc8ea4db6db180217169b53203e9d8ef8d6
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-12 17:19:05 -0500

    Cargamos nuestra base_unida Seleccionamos y renombramos nuestras variables de interes de acuerdo al proyecto. ingreso_bruto jefe_familia edad sexo lengua_materna nivel_edu sector_empleo

--- scripts/Acondicionamiento_Datos.R
+++ scripts/Acondicionamiento_Datos.R
@@ -11,3 +11,32 @@ library(arrow)
 library(janitor)
 library(naniar)
 renv::snapshot()
+
+#2.Carga, selección y renombrado de variables de interés------------------------
+gc()
+# Cargamos la base integrada
+base_unida <- read_parquet("Datos/procesados/enaho_total_2024_050726.parquet")
+
+# Seleccionamos y renombramos de acuerdo al proyecto
+base_acondicionada <- base_unida %>%
+  select(
+    # Keys
+    aÑo,
+    mes,
+    conglome,
+    vivienda,
+    hogar,
+    codperso,
+    ubigeo,
+    dominio,
+    estrato,
+    
+    # Variables
+    ingreso_bruto = ingmo1hd,      # Ingreso disponible mensual del hogar
+    jefe_familia  = p203,          # Parentesco (Filtro Jefe = 1)
+    edad          = p208a,         # Edad 
+    sexo          = p207,          # Sexo
+    lengua_materna = p300a,        # Lengua materna / Idioma
+    nivel_edu     = p301a,         # Nivel educativo alcanzado
+    sector_empleo = p510           # Sector de empleo institucional
+  )
