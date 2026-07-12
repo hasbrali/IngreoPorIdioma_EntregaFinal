02e42bca57789d659777355731e72dbb14a6179f
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-12 17:09:09 -0500

    Ejecutar la unión de módulos por llaves, keys recurrentes en los archivos de la enaho, de consistencia y filtrar la muestra reteniendo únicamente a los jefes de hogar

--- scripts/Carga_Union_Modulos.R
+++ scripts/Carga_Union_Modulos.R
@@ -21,3 +21,14 @@ mod400  <- import("Datos/Crudos/enaho01a-2024-400.dta")
 mod300  <-  import("Datos/Crudos/enaho01a-2024-300.dta") 
 
 sumaria <- import("Datos/Crudos/sumaria-2024-12g.dta") 
+
+#3. Union de bases y filtrado---------------------------------------------------------
+
+keys = c("aÑo", "mes", "conglome", "vivienda", "hogar", "ubigeo", "dominio", "estrato", "codperso")
+
+enaho_total <- mod500 %>%
+  filter(p203 == 1) %>% 
+  left_join(mod400, by = keys) %>%
+  left_join(mod300, by = keys) %>%
+  # Sumaria no tiene 'codperso', intersect() seleccionará automáticamente las 8 llaves del hogar
+  left_join(sumaria, by = intersect(names(.), names(sumaria)))
