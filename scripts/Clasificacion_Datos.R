1d01cbea297a286fd3aa6c064e654ad9bdd71f82
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-12 20:07:22 -0500

    Creación del Script de clasificación de datos

---
+++ scripts/Clasificacion_Datos.R
@@ -0,0 +1,9 @@
+#=====================================================================================
+#Proyecto: INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 12-07-2026
+#Script: CLASIFICACIÓN DE DATOS ENFOQUE: JEFES DE FAMILIA
+#=====================
+#1.Carga de Librerias-----------------------------------------------------------
+library(tidyverse)
+library(arrow)
